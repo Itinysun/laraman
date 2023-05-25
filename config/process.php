f38b3f67c7da2beb58059dd25800f65048ff5793
lcw <860760361@qq.com>
2023-05-25 09:05:33 +0800

    add docs enable monitor auto

--- config/process.php
+++ config/process.php
@@ -25,7 +25,7 @@ return [
             'monitorExtensions' => [
                 'php', 'html', 'htm', 'env'
             ],
-            'enable_file_monitor' => !Worker::$daemonize && DIRECTORY_SEPARATOR === '/',
+            'enable_file_monitor' => true,
             'enable_memory_monitor' => DIRECTORY_SEPARATOR === '/',
         ]
     ],
@@ -35,15 +35,22 @@ return [
             'transport' => 'tcp',
             'context' => [],
             'name' => 'laraman',
+            //process需要的子进程数量，windows无效
             'count' => env('LARAMAN_WEB_WORKERS',0),
+            //运行命令使用的用户及组，windows无效
             'user' => '',
             'group' => '',
+            //是否支持端口复用，如果你需要不同process共用1个端口，请设置为true
             'reusePort' => false,
+            //是否支持平滑重启，如果是持续性任务，请设置为false
             'reloadable' => true,
+            //是否以守护模式运行，windows无效 等效 -d
             'daemonize'=>false,
             'event_loop' => '',
+            //平滑结束超时时间，单位为秒。当进程收到结束、重启信号后，等待当前任务（如果有）执行完毕的最长时间
             'stop_timeout' => 2,
             'runtime_path' => storage_path('laraman'),
+            //全局唯一设定
             'pid_file' => storage_path('laraman') . '/web.pid',
             'status_file' => storage_path('laraman'). '/web.status',
             'stdout_file' => storage_path('laraman') . '/web_stdout.log',
